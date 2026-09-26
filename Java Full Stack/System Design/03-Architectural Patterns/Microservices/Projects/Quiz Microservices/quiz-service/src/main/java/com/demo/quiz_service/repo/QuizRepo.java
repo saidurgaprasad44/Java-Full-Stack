98edf84ed8256d6ce12544fff29ae6cd53f268af
saidurgaprasad44 <saidurgaprasad44@gmail.com>
2026-09-26 19:23:03 +0530

    system design

--- Java Full Stack/System Design/03-Architectural Patterns/Microservices/Projects/Quiz Microservices/quiz-service/src/main/java/com/demo/quiz_service/repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.demo.quiz_service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.demo.quiz_service.model.Quiz;
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz, Integer>{
-}
